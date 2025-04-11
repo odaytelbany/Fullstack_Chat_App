001b04d0aaa958b6f6145a098f1ca62371259ec0
odaytelbany <oday0595814990@gmail.com>
2025-04-11 20:44:33 +0200

    setup tailwind for frontend

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,7 +5,7 @@ import { getAllUsers, getMessages } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users",protectRoute ,getAllUsers);
-router.get("/:id",protectRoute ,getMessages);
+router.get("/:id",protectRoute ,getMessages); 
 router.post("/send/:id",protectRoute ,getMessages);
 
 export default router;
