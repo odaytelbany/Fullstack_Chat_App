682f093f2db3c71eb13ffad1580b0255979da8c8
odaytelbany <oday0595814990@gmail.com>
2025-04-18 20:20:39 +0300

    Create filter by online user functionality

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -50,6 +50,7 @@ export const useChatStore = create((set, get) => ({
     if(!selectedUser) return;
     const socket = useAuthStore.getState().socket;
     socket.on("newMessage", (newMessage) => {
+      if (newMessage.senderId !== selectedUser._id) return;
       set({
         messages: [...get().messages, newMessage]
       })
